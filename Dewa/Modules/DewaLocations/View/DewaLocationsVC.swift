365a129773aa3c02ac4cf058363fed7b0202d1c6
jessjpj <jeslinjpj007@gmail.com>
2022-03-27 11:55:57 +0400

    open details screen from dewa locations list

--- Dewa/Modules/DewaLocations/View/DewaLocationsVC.swift
+++ Dewa/Modules/DewaLocations/View/DewaLocationsVC.swift
@@ -30,12 +30,40 @@ class DewaLocationsVC: UIViewController {
             return
         }
         contentView.collectionView.viewModel = viewModel
+        observeContentView()
     }
 
     func loadLocations() {
         viewModel?.getDewaLocations(completion: { viewModel in
             self.viewModel = viewModel
         })
+    }
+
+    func observeContentView() {
+        contentView.collectionView.itemSelected = { item in
+            self.openDetailScreen(item: item)
+        }
+    }
+
+    func openDetailScreen(item: DewaLocationItem) {
+        let storyBoard = UIStoryboard(name: "LocationDetail", bundle: nil)
+        guard let viewController = storyBoard.instantiateViewController(withIdentifier: LocationDetailVC.identifier()) as? LocationDetailVC else {
+            return
+        }
+        let viewModel = LocationDetailViewModel(
+            latitude: item.lat,
+            longitude: item.lon,
+            location: item.loc,
+            title: item.office,
+            services: nil,
+            distance: item.distance,
+            image: nil)
+        viewController.viewModel = viewModel
+        if let navigationController = self.navigationController {
+            navigationController.pushViewController(viewController, animated: true)
+        } else {
+            self.present(viewController, animated: true, completion: nil)
+        }
     }
 
 }
